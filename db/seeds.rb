750d83cc8322d6eff9df7f150fab001dce85a9b9
jpeterson14 <jpeterson0514@gmail.com>
2020-10-31 19:49:40 -0500

    commit tree 0bfa4440f17f76e31c348e25b3da44bd22f80137

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Jake',last_name:'Peterson')
+Student.create(first_name: 'Brianna',last_name:'Rios')
+Student.create(first_name: 'Leeloo',last_name:'Yokai')
